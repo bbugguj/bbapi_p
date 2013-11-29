a8704b3af3360080059b8a768212a3d8e8d744da
Jaehue Jang <bbugguj@Jaehue-ui-MacBook-Pro.local>
2013-11-29 14:24:44 +0900

    avatar_url을 thumb, medium, original로 전달하도록 UserSerializer 수정

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,11 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :auth_token, :avatar_url
 
   def avatar_url
-    object.avatar.url
+    { 
+      thumb: object.avatar.url(:thumb),
+      medium: object.avatar.url(:medium),
+      original: object.avatar.url
+    }
   end
 
   def auth_token
